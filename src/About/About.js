4efc5f24cf7b183ecccc64c9e0ce80a70121c5fb
Hridoy <rkhridoy68@gmail.com>
2021-10-04 22:49:40 +0600

    final push

--- src/About/About.js
+++ src/About/About.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            <h1 className="text-center my-5">About Us</h1>
+  return (
+    <div>
+      <div className="container">
+        <div class="card text-center my-5">
+          <div class="card-header">
+            <h1>Our Mission and Vision</h1>
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Who We Are At Eduction Buddies</h5>
+            <p class="card-text">
+              With a mission to improve lives through learning, it's no surprise
+              that we place tremendous emphasis on the role of learning in our
+              own lives and work. We understand that learning isn't an
+              interruption of our work or a sign of weakness—it's foundational
+              to our growth, both as individuals and as a business.
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>404!Page Not Found</h1>
+        <div className="text-center">
+            <h1>404 Error</h1>
+            <h3>Page Not Found!</h3>
         </div>
     );
 };
